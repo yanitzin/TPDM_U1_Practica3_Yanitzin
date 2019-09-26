e957170abffcd95517caacfbbc9991d05985951e
yanitzin <yanitzin97@gmail.com>
2019-09-25 20:37:08 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u1_practica3_yanitzin/Main3Activity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.tpdm_u1_practica3_yanitzin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
